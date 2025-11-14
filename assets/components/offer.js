15545a42b1bcf456c738aa966cdcdaa09d42c3a5
Chigtsalmaa <chigtsalmaa31@gmail.com>
2025-11-14 16:28:49 +0800

    kk

--- assets/components/offer.js
+++ assets/components/offer.js
@@ -1,3 +1,6 @@
-offer card n deer drngut hrgdh event bn ene deer hvrglt hiihr zuwshuurh zuwshuurhgu bh gsn 2 songolt bh ba 
-zuwshuurwl bvrtgvvleegu uyd bvrgtgh tsonh grgd 
-zuwhsuurhgu bwl lists ustgna tgd zuwshuursn uyd mnii hvrglt huudsnd ochd tendse yvtsa batalgaajuulj yvna
+// offer card n deer drngut hrgdh event bn ene deer hvrglt hiihr zuwshuurh zuwshuurhgu bh gsn 2 songolt bh ba 
+// zuwshuurwl bvrtgvvleegu uyd bvrgtgh tsonh grgd 
+// zuwhsuurhgu bwl lists ustgna tgd zuwshuursn uyd mnii hvrglt huudsnd ochd tendse yvtsa batalgaajuulj yvna
+
+
+
