92666851587afb8bf5a5213937f5dfb562cf6053
Chigtsanna <chigtsalmaa31@gmail.com>
2025-11-19 20:46:31 +0800

    kk

--- assets/js/my-date.js
+++ assets/js/my-date.js
@@ -2,14 +2,12 @@ document.addEventListener("DOMContentLoaded", () => {
   const dateSel = document.querySelector("input[type='date']");
   const timeSel = document.querySelector("input[type='time']");
 
-  // --- Өнөөдрийн огноо ---
   const today = new Date();
   const yyyy = today.getFullYear();
   const mm = String(today.getMonth() + 1).padStart(2, "0");
   const dd = String(today.getDate()).padStart(2, "0");
   dateSel.value = `${yyyy}-${mm}-${dd}`;
 
-  // --- Одоогийн цаг ---
   const hh = String(today.getHours()).padStart(2, "0");
   const mins = String(today.getMinutes()).padStart(2, "0");
   timeSel.value = `${hh}:${mins}`;
